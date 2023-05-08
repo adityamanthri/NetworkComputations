55e2e6046c970e0bb6e72a0eabec681390020683
KevinHuang78759 <kevin.b.huang007@gmail.com>
2023-05-07 22:58:25 -0500

    Added new decays

--- NetworkGraph/src/SGraph.java
+++ NetworkGraph/src/SGraph.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class SGraph {
     public ArrayList<Integer> nodeList;
     public HashMap<Integer, ArrayList<Integer>> adj;
+
+    public ArrayList<edgeUnordered> edgeList;
     public int size;
 
     /**
@@ -16,6 +18,7 @@ public class SGraph {
         nodeList = new ArrayList<>();
         adj = new HashMap<>();
         size = 0;
+        edgeList = new ArrayList<>();
         edge_weights = new double[0][0];
     }
     public SGraph(int n){
@@ -27,6 +30,33 @@ public class SGraph {
             nodeList.add(i);
             adj.put(i, new ArrayList<>());
         }
+        edgeList = new ArrayList<>();
+    }
+
+    public void makeDense(){
+        for(int i : nodeList){
+            for(int j : nodeList){
+                if(i != j){
+                    edge_weights[i][j] = 1.0;
+                    edge_weights[j][i] = 1.0;
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                    edgeUnordered e  = new edgeUnordered(i, j);
+                    if(!edgeList.contains(e)){
+                        edgeList.add(new edgeUnordered(i, j));
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void ERDecayStep(){
+        Random r = new Random();
+        int indexRemove = r.nextInt(edgeList.size());
+        edgeUnordered removed = edgeList.remove(indexRemove);
+        edge_weights[removed.end1][removed.end2] = 0.0;
+        edge_weights[removed.end2][removed.end1] = 0.0;
     }
 
     public void ERStep(float prob){
@@ -41,6 +71,7 @@ public class SGraph {
                         edge_weights[j][i] = 1.0;
                         adj.get(i).add(j);
                         adj.get(j).add(i);
+                        edgeList.add(new edgeUnordered(i, j));
                     }
                 }
             }
@@ -112,5 +143,22 @@ public class SGraph {
             aggregate.addAll(comp.nodeList);
         }
         return components;
+    }
+}
+
+class edgeUnordered{
+    int end1;
+    int end2;
+    public edgeUnordered(int a, int b){
+        end1 = a;
+        end2 = b;
+    }
+
+    public boolean isEqual(Object other){
+        if(other  instanceof edgeUnordered){
+            edgeUnordered O = (edgeUnordered) other;
+            return (end1 == O.end1 && end2 == O.end2) || (end2 == O.end1 && end1 == O.end2);
+        }
+        return false;
     }
 }
