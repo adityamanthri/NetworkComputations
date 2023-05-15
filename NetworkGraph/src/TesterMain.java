ef4da2e544e3ee5f98ec45d5cbcd29818d32fe5d
KevinHuang78759 <kevin.b.huang007@gmail.com>
2023-05-15 10:30:35 -0500

    Added new decays

--- NetworkGraph/src/TesterMain.java
+++ NetworkGraph/src/TesterMain.java
@@ -11,20 +11,39 @@ public class TesterMain {
 //        }
 //    }
 
-        double[][] m = new double[][]{{4, -30, 60, -35}, {-30, 300, -675, 420}, {60, -675, 1620, -1050}, {-35, 420, -1050, 700}};
-        EigenvalueDecomposition ed = new EigenvalueDecomposition(m);
-        double[][] EV = ed.getV();
-        double[] v = ed.getRealEigenvalues();
-        for(int i =0; i < v.length; ++i){
-            System.out.print(v[i] + " ");
-        }
-        System.out.println("\n");
-        for(int i = 0; i < EV.length; ++i){
-            for(int j = 0; j < EV[i].length; ++j){
-                System.out.print(EV[i][j] + " ");
+//        double[][] m = new double[][]{{4, -30, 60, -35}, {-30, 300, -675, 420}, {60, -675, 1620, -1050}, {-35, 420, -1050, 700}};
+//        EigenvalueDecomposition ed = new EigenvalueDecomposition(m);
+//        double[][] EV = ed.getV();
+//        double[] v = ed.getRealEigenvalues();
+//        for(int i =0; i < v.length; ++i){
+//            System.out.print(v[i] + " ");
+//        }
+//        System.out.println("\n");
+//        for(int i = 0; i < EV.length; ++i){
+//            for(int j = 0; j < EV[i].length; ++j){
+//                System.out.print(EV[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        float hi = 1f;
+        float lo = 0f;
+        int d = 17;
+        int n = 100;
+        for(int k = 1; k <= n; ++k){
+            float x = (hi + lo)/2f;
+            float fx = (3 * x - 2)* (3*x - 2);
+            System.out.println(x + " " + fx);
+            if (fx > 0){
+                hi = x;
             }
-            System.out.println();
+            else{
+                lo = x;
+            }
         }
+        System.out.println((hi + lo)/2f);
+
+
     }
 
 }
